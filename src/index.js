6198eb8066800843db9055011e880f50c33c8ca3
namitha9817 <namithasunil17@gmail.com>
2024-09-09 13:44:53 +0100

    Initialize React project and set up basic routing
    - Set up the React project using create-react-app.
    - Installed necessary dependencies (react-router-dom).
    - Created Header component with basic navigation links.
    - Configured routing for the landing page and other potential routes.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import App from './App';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />,
+  document.getElementById('root')
+);
+
