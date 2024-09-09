5c2f98745695ce6a1fb8a0bff58a29215939f264
namitha9817 <namithasunil17@gmail.com>
2024-09-09 17:49:43 +0100

    Add movie list and create components for MovieCard and MovieList
    - Created the MovieList component to display a list of movies fetched from the API.
    - Developed the MovieCard component to display individual movie details, including title, image, rating, and release date.
    - Integrated the components into the homepage for structured and modular display of movies.

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,6 +3,7 @@ import "./home.css"
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/movieList";
 
 const Home = () => {
 
@@ -46,6 +47,7 @@ const Home = () => {
                         ))
                     }
                 </Carousel>
+                <MovieList />
             </div>
         </>
     )
